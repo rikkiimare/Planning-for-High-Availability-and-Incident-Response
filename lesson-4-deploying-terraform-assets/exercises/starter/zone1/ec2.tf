0c274fb1abbe4e013bcc0c7243dd5065eaf593fc
Richard Irving <richard.irving3@dwp.gov.uk>
2022-11-11 08:51:51 +0000

    Amendments to Lesson 4 ec2.tf & 5 _config.tf

--- lesson-4-deploying-terraform-assets/exercises/starter/zone1/ec2.tf
+++ lesson-4-deploying-terraform-assets/exercises/starter/zone1/ec2.tf
@@ -1,10 +1,10 @@
   module "project_ec2" {
-   source             = "./modules/ec2"
-   instance_count     = 2
+   source             = "./modules/ec2""./modules/ec2"
+   instance_count     = 22
    name               = local.name
    account            = data.aws_caller_identity.current.account_id
    aws_ami            = "ami-0b9064170e32bde34"
    private_subnet_ids = module.vpc.private_subnet_ids
-   public_subnet_ids  = module.vpc.public_subnet_ids
-   vpc_id             = module.vpc.vpc_id
+   public_subnet_ids  = module.vpc.public_subnet_idsmodule.vpc.public_subnet_ids
+   vpc_id             = module.vpc.vpc_idmodule.vpc.vpc_id
  }
