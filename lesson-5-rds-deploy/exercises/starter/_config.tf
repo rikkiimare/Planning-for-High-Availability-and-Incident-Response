cf640809d0023d6fd0121364f3635aaae2e87b8e
Richard Irving <richard.irving3@dwp.gov.uk>
2022-11-11 08:09:09 +0000

    Changes up to & including lesson-4

--- lesson-5-rds-deploy/exercises/starter/_config.tf
+++ lesson-5-rds-deploy/exercises/starter/_config.tf
@@ -1,6 +1,6 @@
 terraform {
    backend "s3" {
-     bucket = "udacity-tf-travis5"
+     bucket = "udacity-tf-rirving"
      key    = "terraform/terraform.tfstate"
      region = "us-east-2"
    }
@@ -8,7 +8,7 @@ terraform {
 
  provider "aws" {
    region = "us-east-2"
-   
+
    default_tags {
      tags = local.tags
    }
