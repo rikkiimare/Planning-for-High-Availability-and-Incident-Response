24de7e8e625b4ddd8224bf024e6c40b6ca666deb
Richard Irving <richard.irving3@dwp.gov.uk>
2022-12-16 13:58:03 +0000

    amend aws required_providers in  _config.tf

--- project/starter-code/zone1/_config.tf
+++ project/starter-code/zone1/_config.tf
@@ -4,9 +4,17 @@ terraform {
      key    = "terraform/terraform.tfstate"
      region = "us-east-2"
    }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
  }
 
- provider "aws" {
+provider "aws" {
    region = "us-east-2"
    
    default_tags {
