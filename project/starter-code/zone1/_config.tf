a06418e009e8418580c60ea927780321ae068988
Richard Irving <richard.irving3@dwp.gov.uk>
2022-12-19 08:28:14 +0000

    required_providers  _config.tf

--- project/starter-code/zone1/_config.tf
+++ project/starter-code/zone1/_config.tf
@@ -1,4 +1,11 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+   }
+
    backend "s3" {
      bucket = "udacity-tf-irvingr"
      key    = "terraform/terraform.tfstate"
