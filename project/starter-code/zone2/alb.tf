718957826b977fbb8976261e9e560d3f886bcf0d
Richard Irving <richard.irving3@dwp.gov.uk>
2022-12-20 08:44:16 +0000

    Amend '.vpc.public_subnet_ids' from '.vpc_west.public_subnet_id

--- project/starter-code/zone2/alb.tf
+++ project/starter-code/zone2/alb.tf
@@ -3,6 +3,6 @@ module "project_alb" {
    source             = "./modules/alb"
    ec2                = module.project_ec2.aws_instance
    ec2_sg             = module.project_ec2.ec2_sg
-   subnet_id          = module.vpc_west.public_subnet_ids
-   vpc_id             = module.vpc_west.vpc_id
+   subnet_id          = module.vpc.public_subnet_ids
+   vpc_id             = module.vpc.vpc_id
  }
