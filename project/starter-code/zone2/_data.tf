4b5eca30981a40987a06e65abccd33dac37189ee
Richard Irving <richard.irving3@dwp.gov.uk>
2022-12-20 09:08:59 +0000

    Amend bucket name to tf-irvingr in _data.tf in Zone2

--- project/starter-code/zone2/_data.tf
+++ project/starter-code/zone2/_data.tf
@@ -19,7 +19,7 @@ data "aws_caller_identity" "current" {}
 data "terraform_remote_state" "vpc" {
   backend = "s3" 
   config = {
-     bucket = "udacity-tf-rirving"
+     bucket = "udacity-tf-irvingr"
      key    = "terraform/terraform.tfstate"
      region = "us-east-2"
    }
