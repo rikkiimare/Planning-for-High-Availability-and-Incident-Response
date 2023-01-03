ad548f7d6673c8531068fbf483460af7dd4c44d6
Richard Irving <richard.irving3@dwp.gov.uk>
2023-01-03 08:09:59 +0000

    amended the _config.tf file with bucket name

--- project/starter-code/zone1/_config.tf
+++ project/starter-code/zone1/_config.tf
@@ -7,7 +7,7 @@ terraform {
    }
 
    backend "s3" {
-     bucket = "udacity-tf-irvingr"
+     bucket = "udacity-tf-rirving"
      key    = "terraform/terraform.tfstate"
      region = "us-east-2"
    }
